2ad35960d14be9cfe870b166e7c051014f37dde9
camil <camil@DESKTOP-IED9PJH>
2023-07-31 17:58:09 +0900

    2d comparison with luca article

--- PlottingFunctions/plotkernel.m
+++ PlottingFunctions/plotkernel.m
@@ -1,58 +1,92 @@
-% Step 1: Define the functions
-L2 = @(x) x.^2;
+% Step 1: Define all the  functions
+s=1;
+L2 = @(x) x.^2/2;
 L1 = @(x) abs(x);
-
-Charbonnier=@(x) sqrt(x.^2+1)-1;
-Cauchy=@(x)  log(1+x.^2);
-Geman=@(x)  (x.^2/2)./(1+x.^2);
-
+Charbonnier=@(x,s) sqrt((x./s).^2+1)-1; 
+Fair=@(x,s) s.^2.*(abs(x)./s-log(1+abs(x)./s));
+Cauchy=@(x,s)  s.^2./2.*log(1+(x./s).^2);
+Geman=@(x,s)  (x.^2/2)./(s+x.^2);
+Welsch=@(x,s)  s.^2./2.*(1-exp(-(x./s).^2));
 
 % Step 2: Define the range of x-values
-x = linspace(-10, 10, 100); % Range from -π to π with 100 points
+x = linspace(-4, 4); % 
 
 % Step 3: Evaluate the functions
 y1 = L1(x);
 y2 = L2(x);
-y3= huber(x);
-y4=Charbonnier(x);
-y5=Cauchy(x);
-y6=Geman(x);
+y3= Charbonnier(x,s);
+y4=huber(x,s);
+y5=Fair(x,s);
+y6=Cauchy(x,s);
+y7=Geman(x,s);
+y8=Welsch(x,s);
+y9=Threshold(x,s);
+y10=Tukey(x,s);
 
 % Step 4: Plot the functions
+plot(x, y1);  % Blue lineplot(x, y1, 'blue-');  
+hold on;
+plot(x, y2);  % Blue line
+hold on;
+plot(x, y3);  % Blue line
+hold on;
+plot(x, y4);  % Blue line
+hold on;
+plot(x, y5);  % Blue line
+hold on;
+plot(x, y6);  % Blue lineplot(x, y1, 'blue-');  
+hold on;
+plot(x, y7);  % Blue line
+hold on;
+plot(x, y8);  % Blue line
+hold on;
+plot(x, y9);  % Blue line
+hold on;
+plot(x, y10);  % Blue line
 
-plot(x, y1, 'blue-');  % Blue line
-hold on;
-plot(x, y2, 'red-');  % Red line 
-hold on;
-plot(x,y3, 'color',[0.4660 0.6740 0.1880]);  % Blue line
-hold on;
-plot(x, y4 ,'magenta-');  % Red line 
-hold on;
-plot(x, y5, 'black-');  % Blue line
-hold on;
-plot(x, y6, 'color', [0.3010 0.7450 0.9330]);  % Red line $
-%plot(x, y6, 'red-');  % Red line $
 
-hold on;
-% Add labels and a legend
 xlabel('x');
 ylabel('\rho');
-legend('L1', 'L2','Huber','Charbonnier','Cauchy','Geman');
-
+legend('L1','L2','Charbonnier','huber','Fair','Cauchy','Geman','Welsch','Threshold','Tukey')
 % Optionally, you can add a title
-title('Kernel function behaviour');
+
 
 % Optionally, you can adjust the axis limits if needed
-xlim([-11, 11]);
-ylim([0, 10]);
+xlim([-2, 2]);
+ylim([0, 2]);
 
 % Turn off the hold
 hold off;
 
-function y=huber(x)
-    if abs(x)<1
-        y=x^2;
-    else 
-        y=2*abs(x)-1;
+function y=huber(x,s)
+    
+    for i=1:length(x)
+        if abs(x(i))<=s
+            y(i)=x(i)^2/2;
+        else 
+            y(i)=s*(abs(x(i))-s/2);
+        end
     end
+end
+
+function y=Threshold(x,s)
+    
+    for i=1:length(x)
+        if abs(x(i))<=s
+            y(i)=x(i)^2/2;
+        else 
+            y(i)=s^2/2;
+        end
+    end
+end
+
+function y=Tukey(x,s)
+    
+    for i=1:length(x)
+        if abs(x(i))<=s
+            y(i)=s^2*(1-(1-(x(i)/s)^2)^3);
+        else 
+            y(i)=s^2/6;
+        end
+    end 
 end
